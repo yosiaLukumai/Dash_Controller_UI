de836b1338d881ddb328c16a5321a603c04f1213
yosiaLukumai <yosialukumai@gmail.com>
2025-01-16 11:53:27 -0800

    pushing changes for the new server

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
-    API_URL: import.meta.env.VITE_API_URL || 'http://143.198.62.200:3710',
-    SOCKET_URL: import.meta.env.VITE_SOCKET_URL || 'ws://143.198.62.200:3710',
+    API_URL: import.meta.env.VITE_API_URL || 'http://45.79.206.183:3710',
+    SOCKET_URL: import.meta.env.VITE_SOCKET_URL || 'ws://45.79.206.183:3710',
     MAP_API_KEY: import.meta.env.VITE_MAP_API_KEY || '',
     MAP_STYLE: import.meta.env.VITE_MapStyleID
     // Add other configuration variables as needed
